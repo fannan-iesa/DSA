adbc70a46554d30edefdb4beb35572aaa9d267be
unknown <b24bs2205@iitj.ac.in>
2026-08-29 04:09:21 +0530

    Remove Outermost paranthesis Problem done

---
+++ STRINGS/easy/remove(.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+class Solution {
+public:
+    std::string removeOuterParentheses(std::string s) {
+        std::string a="";
+        int c=0;
+
+        for(int i:s){
+            if(i=='('){
+                if(c>0){
+                    a+=i;
+                }
+                c++;
+            }else if(i==')'){
+                c--;
+                if(c>0){
+                    a+=i;
+                }
+            }
+        }
+        return a;
+    }
+};
+int main() {
+    Solution solver;
+
+    std::string s = "(()())(())";
+    std::string result = solver.removeOuterParentheses(s);
+
+    std::cout << "String after removing outer parentheses: " << result << std::endl;
+
+    return 0;
+}
